d4557a30ac50a5d95d3a02ecb1d411bfffec7e19
uzmanqamar <36234054+uzmanqamar@users.noreply.github.com>
2020-07-29 15:02:06 +0500

    customized CSS file Added
    customized CSS file Added

--- Sources/LatticeBoltzmann/main.swift
+++ Sources/LatticeBoltzmann/main.swift
@@ -22,11 +22,156 @@ struct NiallOBroin: Website {
     var imagePath: Path? { nil }
 //    var socialMediaLinks: [SocialMediaLink] { [.location, .email, .linkedIn, .github, .twitter] }
 }
-
+private extension Node where Context == HTML.BodyContext {
+    static func wrapper(_ nodes: Node...) -> Node {
+        .div(.class("wrapper"), .group(nodes))
+    }//wrapper
+    
+    static func itemList<T: Website>(for items: [Item<T>], on site: T) -> Node {
+        return .ul(
+            .class("item-list"),
+            .forEach(items) { item in
+                .li(.article(
+                    .h1(.a(
+                        .href(item.path),
+                        .text(item.title)
+                        )),
+                    .p(.text(item.description))
+                    ))
+            }
+        )
+    }//itemList
+}//ext
+struct MyHtmlFactory<Site: Website>: HTMLFactory{
+    func makeIndexHTML(for index: Index,
+                       context: PublishingContext<Site>) throws -> HTML {
+        let items = context.allItems(sortedBy: \.date, order: .descending)
+        return HTML( .head(for: index, on: context.site),//head
+            .body(
+                .header(
+                    .wrapper(
+                        .nav(
+                            .class("website-name"),
+                            .a(.href("/"),
+                               
+                               .text(context.site.name)),
+                            
+                            .ul(
+                                .forEach(items) { item in
+                                    .if(item.title != "first-post" && item.title != "Research",
+                                        
+                                        .li(.article(
+                                            .a(.href(item.path),
+                                               .text(item.title))
+                                            )))
+                                    
+                                    
+                                    
+                                }
+                            )
+                        )
+                    )
+                ),
+                .hr(),
+                .wrapper(
+                    .ul(
+                        .class("listing"),
+                        .forEach(
+                            context.allItems(sortedBy: \.date, order: .descending)
+                        ){ item in
+                            .if(item.title == "first-post",
+                            .li(
+                                .class("non-listing"),
+                                .article(
+                                    //                                    .h1(.text(item.title)),
+                                    //                                    .p(.text(item.description)),
+                                    
+                                    .contentBody(item.body)
+                                )//artical
+                            )//li
+                            )
+                        }
+                    )//ul
+                )//wrapper
+            )//body
+            
+        )//html
+    }
+    
+    func makeSectionHTML(for section: Section<Site>,
+                         context: PublishingContext<Site>) throws -> HTML {
+        HTML( .head(for: section, on: context.site)
+        )
+    }
+    
+    func makeItemHTML(for item: Item<Site>,
+                      context: PublishingContext<Site>) throws -> HTML {
+         let items = context.allItems(sortedBy: \.date, order: .descending)
+//        let items = context.allItems(sortedBy: \.date, order: .descending)
+        
+       return HTML( .head(for: item, on: context.site),
+            .body(
+                .header(
+                    .wrapper(
+                        .nav(
+                            .class("website-name"),
+                            .a(.href("/"),
+                               
+                               .text(context.site.name)),
+                            
+                            .ul(
+                                
+                                //                                                    .id("menuItems"),
+                                .forEach(items) { item in
+                                    .if(item.title != "first-post",
+                                        .li(.article(
+                                            .a(.href(item.path),
+                                               .text(item.title))
+                                            )))
+                                }
+                            )
+                        )
+                    )
+                ),
+                .hr(),
+                .wrapper(
+                    .article(
+//                        .h1(item.title),
+                        .contentBody(item.body)
+                    )
+                )
+            )
+            
+        )
+        
+    }
+    
+    func makePageHTML(for page: Page,
+                      context: PublishingContext<Site>) throws -> HTML {
+        try makeIndexHTML(for: context.index, context: context)
+    }
+    
+    func makeTagListHTML(for page: TagListPage,
+                         context: PublishingContext<Site>) throws -> HTML? {
+        nil
+    }
+    
+    func makeTagDetailsHTML(for page: TagDetailsPage,
+                            context: PublishingContext<Site>) throws -> HTML? {
+        nil
+    }
+}
+extension Theme{
+    static var myTheme :Theme{
+        Theme(
+            htmlFactory: MyHtmlFactory(),
+            resourcePaths: ["Resources/MyTheme/styles.css"]
+        )
+    }
+}
 // This will generate your website using the built-in Foundation theme:
-try NiallOBroin().publish(withTheme: .foundation,
-                          deployedUsing: .gitHub("TurbulentDynamics/LatticeBoltzmann.github.io.publish"))
+try NiallOBroin().publish(withTheme: .myTheme)
 
 
-
+// deployedUsing: .gitHub("TurbulentDynamics/LatticeBoltzmann.github.io.publish")
 
